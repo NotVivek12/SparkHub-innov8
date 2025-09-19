fbf4e94de7f022143a44be3ce5ea719f27b5eb74
Pavan-Hosatti <pavu586@gmail.com>
2025-09-19 18:01:49 +0000

    Added frontend folder structure and initial files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hello, SparkHub!
+    </div>
+  );
+}
+
+export default App;
